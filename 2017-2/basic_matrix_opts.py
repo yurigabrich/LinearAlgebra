9b6a9c15f7ee7db6a3573ce63dd06e645faee2ab
Yuri <yuribgabrich@gmail.com>
2017-03-23 21:06:07 -0300

    Create basic_matrix_opts.py

---
+++ 2017-2/basic_matrix_opts.py
@@ -0,0 +1,33 @@
+class matrix_opts:
+	def _init_(self, A, B):
+		'''
+		Initiates the statement with matrix A and matrix B.
+		'''
+		self.A = A
+		self.B = B
+
+	def sum(self.A, self.B):
+		'''
+		Makes a sum between matrix A and B by column indexing.
+		'''translate
+		# check if both matrices have the same size
+		if (len(self.A) != len (self.B)) || (len(self.A[0]) != len(self.B[0])):
+			return "Can't sum A and B. Check their dimensions."
+		else:
+			AsumB = []
+			for j in range(len(self.A[0])):
+				for i in range(len(self.A)):
+					AsumB.extend( self.A[i][j] + self.B[i][j] )
+				AsumB.append(0)
+			
+			return AsumB
+			
+	def times(self.A, self.B):
+		'''
+		Multiplication between matrices A and B.
+		'''
+		# check if matrices satify product condition
+		if (len(self.A[0]) != len(self.B)):
+			return "Can't product A and B. Check their dimensions."
+		else:
+		
