4abe0f1b611fd03a2b5d347740ad4bb7a8e27974
Yuri <yuribgabrich@gmail.com>
2019-02-24 17:24:00 -0300

    Update QR_GS.py

--- T3/QR_GS.py
+++ T3/QR_GS.py
@@ -1,3 +1,11 @@
+# _________________________________________________________________
+# SPDX-License-Identifier: MIT License
+# For more information check at: https://spdx.org/licenses/MIT.html
+# 
+# Copyright (C) 2017
+# Yuri Bastos Gabrich <yuribgabrich[at]gmail.com>
+# _________________________________________________________________
+
 def QR_GS(A):
     '''
     Calculates the QR decomposition by Gram-Schimdt method.
